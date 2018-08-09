5acc839611113f94728c8c03ee19e7848cf3adf3
motoyarko <motoyarko@gmail.com>
2018-08-09 23:21:47 +0300

    refactor code for use pytest: add fixture for pytest remove unittest sintax update test functions for use fixture "app"

--- test_add_group.py
+++ test_add_group.py
@@ -1,36 +1,25 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-# from selenium.webdriver.common.action_chains import ActionChains
-import unittest
+
+import pytest
 from group import Group
 from application import Application
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-    
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_new_group(Group(name="test1", header="header", footer="footer"))
-        self.app.logout()
-
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_new_group(Group(name="", header="", footer=""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_new_group(Group(name="test1", header="header", footer="footer"))
+    app.logout()
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_new_group(Group(name="", header="", footer=""))
+    app.logout()
+
