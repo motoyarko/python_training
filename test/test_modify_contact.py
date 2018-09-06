52d6806cae790112fd66719cc21b08873032b3f5
motoyarko <motoyarko@gmail.com>
2018-09-06 22:59:06 +0300

    add several tests with updating not all fields for contact

---
+++ test/test_modify_contact.py
@@ -0,0 +1,26 @@
+
+from model.contact import Contact
+
+
+def test_modify_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first_contact(Contact(middle_name="middleName_Updated", first_name="firstName_Updated", last_name="lastName_Updated", nick_name="nickName_Updated",
+                                             image="C:\\Users\\motoy\\Downloads\\Starinnyi-velosiped-s-bol1shim-perednim-kolesom.jpg",
+                                             title="title_Updated", company="company_Updated", address="address_Updated", home_telephone="homeTelephone_Updated", mobile_telephone="mobileTelephone_Updated",
+                                             work_telephone="workTelephone_Updated", fax_telephone="faxTelephone_Updated", email="email1_Updated@email.com",
+                                             email2="email2_Updated@email.com", email3="email3_Updated@email.com", homepage="homepage_Updated.com",
+                                             birth_date=24, birth_month=8, birth_year=1989, anniversary_year=1989, anniversary_day=6, anniversary_month=8,
+                                             group=4, address2="address2_Updated", home="home_Updated", notes="notes_Updated"))
+    app.session.logout()
+
+
+def test_modify_contact_middle_name(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first_contact(Contact(middle_name="middleName_Updated"))
+    app.session.logout()
+
+
+def test_modify_contact_first_name(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first_contact(Contact(first_name="firstName_Updated"))
+    app.session.logout()
