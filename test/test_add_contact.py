cd5ba5cf75d65532fe3947cdfd5ef42b8182622d
motoyarko <motoyarko@gmail.com>
2018-09-06 23:33:36 +0300

    default values for creating contact were changed

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,6 +10,6 @@ def test_add_contact(app):
                        title="title", company="company", address="address", home_telephone="homeTelephone", mobile_telephone="mobileTelephone",
                        work_telephone="workTelephone", fax_telephone="faxTelephone", email="email1@email.com",
                        email2="email2@email.com", email3="email3@email.com", homepage="homepage.com",
-                       birth_date=23, birth_month=6, birth_year=1988, anniversary_year=1988, anniversary_day=5, anniversary_month=6,
+                       birth_date=24, birth_month=7, birth_year=1988, anniversary_year=1989, anniversary_day=2, anniversary_month=3,
                        group=5, address2="address2", home="home", notes="notes"))
     app.session.logout()
