e4267868b8f0c7d872cfcdc8b529c4fc9aebf303
motoyarko <motoyarko@gmail.com>
2018-09-10 23:05:01 +0300

    remove login, logout move login, logout to conftest.py

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,7 +4,6 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(middle_name="middleName", first_name="firstName", last_name="lastName", nick_name="nickName",
                        image="C:\\Users\\motoy\\Downloads\\Starinnyi-velosiped-s-bol1shim-perednim-kolesom.jpg",
                        title="title", company="company", address="address", home_telephone="homeTelephone", mobile_telephone="mobileTelephone",
@@ -12,4 +11,3 @@ def test_add_contact(app):
                        email2="email2@email.com", email3="email3@email.com", homepage="homepage.com",
                        birth_date=24, birth_month=7, birth_year=1988, anniversary_year=1989, anniversary_day=2, anniversary_month=3,
                        group=5, address2="address2", home="home", notes="notes"))
-    app.session.logout()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,18 +2,12 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="New group updated"))
-    app.session.logout()
 
 
 def test_modify_group_header(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(header="New header updated"))
-    app.session.logout()
 
 
 def test_modify_group_footer(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(footer="New footer updated"))
-    app.session.logout()
