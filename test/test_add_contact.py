94d60ca52f7c978f6b620ed313fdfb473dc80a7c
motoyarko <motoyarko@gmail.com>
2018-09-12 01:21:02 +0300

    update tests with checking fields of contacts and groups before and after add/modify the lists of groups/contacts are sorted before check

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,12 +5,15 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create(Contact(middle_name="middleName", first_name="firstName", last_name="lastName", nick_name="nickName",
+    contact = Contact(middle_name="middleName", first_name="firstName", last_name="lastName", nick_name="nickName",
                        image="C:\\Users\\motoy\\Downloads\\Starinnyi-velosiped-s-bol1shim-perednim-kolesom.jpg",
                        title="title", company="company", address="address", home_telephone="homeTelephone", mobile_telephone="mobileTelephone",
                        work_telephone="workTelephone", fax_telephone="faxTelephone", email="email1@email.com",
                        email2="email2@email.com", email3="email3@email.com", homepage="homepage.com",
                        birth_date=24, birth_month=7, birth_year=1988, anniversary_year=1989, anniversary_day=2, anniversary_month=3,
-                       group=5, address2="address2", home="home", notes="notes"))
+                       group=5, address2="address2", home="home", notes="notes")
+    app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,15 +5,19 @@ def test_modify_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="createdForUpdateContact"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(middle_name="middleName_Updated", first_name="firstName_Updated", last_name="lastName_Updated", nick_name="nickName_Updated",
+    contact = Contact(middle_name="middleName_Updated", first_name="firstName_Updated", last_name="lastName_Updated", nick_name="nickName_Updated",
                                              image="C:\\Users\\motoy\\Downloads\\Starinnyi-velosiped-s-bol1shim-perednim-kolesom.jpg",
                                              title="title_Updated", company="company_Updated", address="address_Updated", home_telephone="homeTelephone_Updated", mobile_telephone="mobileTelephone_Updated",
                                              work_telephone="workTelephone_Updated", fax_telephone="faxTelephone_Updated", email="email1_Updated@email.com",
                                              email2="email2_Updated@email.com", email3="email3_Updated@email.com", homepage="homepage_Updated.com",
                                              birth_date=24, birth_month=8, birth_year=1989, anniversary_year=1989, anniversary_day=6, anniversary_month=8,
-                                             group=4, address2="address2_Updated", home="home_Updated", notes="notes_Updated"))
+                                             group=4, address2="address2_Updated", home="home_Updated", notes="notes_Updated")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_modify_contact_middle_name(app):
