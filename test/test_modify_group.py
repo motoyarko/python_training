81deeac2e5ef89b1aceba002695a9d40d954170f
motoyarko <motoyarko@gmail.com>
2018-09-11 22:13:39 +0300

    update tests for groups tests new tests check the amount of groups before and after CRUD group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,16 +4,25 @@ from model.group import Group
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="createdForModify"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="New group updated"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header="createdForModify"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="New header updated"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(footer="createdForModify"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(footer="New footer updated"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
