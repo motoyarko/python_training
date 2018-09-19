63103ba86ca1a84f923fc0233f98a8911bcb58a2
motoyarko <motoyarko@gmail.com>
2018-09-19 15:50:38 +0300

    create new method for get logged user name. fix text layout in test_modify_group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,6 +1,7 @@
 from model.group import Group
 from random import randrange
 
+
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="createdForModify"))
