94c85d1d80d0ffafd04cf0c31e6960d97fadeacd
motoyarko <motoyarko@gmail.com>
2018-09-24 17:23:47 +0300

    small refactor

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -22,7 +22,6 @@ testdata = [
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    #group = Group(name="test1", header="header", footer="footer") // Deprecated
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
