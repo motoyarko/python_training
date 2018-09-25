cfef5318d345b043ed423508f6c3a46fbe2a4226
motoyarko <motoyarko@gmail.com>
2018-09-25 13:13:24 +0300

    Remove extra params for __init__ method in Application add functionality with loading application data from json

--- fixture/application.py
+++ fixture/application.py
@@ -5,7 +5,7 @@ from fixture.group import GroupHelper
 
 
 class Application:
-    def __init__(self, browser, base_url, username, password):
+    def __init__(self, browser, base_url):
         if browser == "firefox":
             self.wd = webdriver.Firefox(capabilities={"marionette": False})
         elif browser == "chrome":
@@ -18,8 +18,6 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.base_url = base_url
-        self.username = username
-        self.password = password
 
     def is_valid(self):
         try:
